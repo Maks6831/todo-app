e5a042bf9b23230d97f91af7a8c592a7bf32e8d6
Maks6831 <ma.khan6971@gmail.com>
2023-09-15 20:44:21 +0100

    added task header title and back button

--- src/pages/AddTask.js
+++ src/pages/AddTask.js
@@ -1,7 +1,32 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import '../styles/AddTask.css';
+import { BsArrowLeft } from 'react-icons/bs';
+
 
 export const AddTask = () => {
   return (
-    <div>AddTask</div>
+    <div className='addTask-container'>
+      <div className='task-inside'>
+        <div className='task-header'>
+          <div className='home-link'>
+            <Link to='/'>
+              <BsArrowLeft size={25}/>
+            </Link>
+          </div>
+          <h1>Add New Task</h1>
+        </div>
+        <div className='task-container'>
+          <div className='t-taskname'></div>
+          <div className='t-priority'></div>
+          <div className='t-complexity'></div>
+          <div className='t-time'></div>
+          <div className='t-checklist'></div>
+          <div className='t-tags'></div>
+          <div className='t-save-button'></div>
+        </div>
+
+      </div>
+    </div>
   )
 }
